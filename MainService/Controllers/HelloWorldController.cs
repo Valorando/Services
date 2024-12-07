6704914f0daf5a9b69f4ef7c046dbbe4950805a8
DESKTOP-SVLI0O8\User <sokol3389@gmail.com>
2024-12-07 23:10:26 +0200

    add new controller

---
+++ MainService/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MainService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloWorldController : ControllerBase
+    {
+        // GET: api/HelloWorld
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello, World!");
+        }
+    }
+}
